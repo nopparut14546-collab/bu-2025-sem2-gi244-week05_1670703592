0992d5cf32f76bf041231ba4ed6d0529205ef46f
nopparut14546-collab <nopparut.14546@gmail.com>
2026-02-13 16:54:55 +0700

    Update HealthV2.cs

--- Assets/Scripts/HealthV2.cs
+++ Assets/Scripts/HealthV2.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class HealthV2 : MonoBehaviour
 {
+    public int maxHP = 100;
+    private int accumDamage = 0;
+
     public void TakeDamage(int damage)
     {
-
+        accumDamage += damage;
+        if (accumDamage >= maxHP)
+        {
+            Destroy(gameObject);
+        }
     }
 }
